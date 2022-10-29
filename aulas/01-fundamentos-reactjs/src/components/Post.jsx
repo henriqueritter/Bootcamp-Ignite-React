0a5c25a3e2b996903f1964268e93e80d53b77869
henriqueritter <henrique-ritter@hotmail.com>
2022-10-29 13:41:47 -0300

    feat: init Post component HTML structure

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -2,11 +2,25 @@ import styles from './Post.module.css';
 
 export function Post(props) {
   return (
-    <div>
-      <span>{props.author}</span>
-      <p>
-        {props.content}
-      </p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/henriqueritter.png" />
+          <div className={styles.authorInfo}>
+            <strong>Henrique Ritter</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="29 de outubro às 13:34h" dateTime="2022-10-29 13:34:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galera</p>
+        <p> Acabei de postar um projeto</p>
+        <p> <a href="">https://www.google.com </a></p>
+        <p> <a href=""> #projeto #ignite </a></p>
+      </div>
+    </article>
   )
 }
