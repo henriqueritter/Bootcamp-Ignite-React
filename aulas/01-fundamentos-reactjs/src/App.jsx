3af48d0ea3de0ae9cc24d97344e256f76c55277d
henriqueritter <henrique-ritter@hotmail.com>
2022-10-28 21:35:45 -0300

    feat: add Header component

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,17 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './global.css';
 
 export function App() {
   return (
-    <Post
-      author="Henrique"
-      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam repudiandae iusto, incidunt enim consequuntur dolorem eaque, quisquam totam, ad qui fugiat earum nam inventore est minus! Fugiat quasi in error."
-    />
+    <div>
+      <Header />
+
+      <Post
+        author="Henrique"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam repudiandae iusto, incidunt enim consequuntur dolorem eaque, quisquam totam, ad qui fugiat earum nam inventore est minus! Fugiat quasi in error."
+      />
+    </div>
   )
 }
--- aulas/01-fundamentos-reactjs/src/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/Post.jsx
@@ -1,13 +1,13 @@
 export function Post(props) {
   console.log(props);
   return (
-    <>
+    <div>
       <strong>
         {props.author}
       </strong>
       <p>
         {props.content}
       </p>
-    </>
+    </div>
   )
 }
