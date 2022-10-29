cf608e96188287b3def1be456c5646ab771f6101
henriqueritter <henrique-ritter@hotmail.com>
2022-10-28 22:41:53 -0300

    feat: init Sidebar component

--- aulas/01-fundamentos-reactjs/src/App.jsx
+++ aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,17 +1,26 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
+import styles from './App.module.css';
 
 import './global.css';
+
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Henrique"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam repudiandae iusto, incidunt enim consequuntur dolorem eaque, quisquam totam, ad qui fugiat earum nam inventore est minus! Fugiat quasi in error."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Henrique"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam repudiandae iusto, incidunt enim consequuntur dolorem eaque, quisquam totam, ad qui fugiat earum nam inventore est minus! Fugiat quasi in error."
+          />
+        </main>
+      </div>
+
     </div>
   )
 }
