220b288fd039c2c51057ebef0e61417d80d2827e
henriqueritter <henrique-ritter@hotmail.com>
2022-10-31 18:57:15 -0300

    feat: created Comment component HTML structure

---
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,28 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/henriqueritter.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Henrique Ritter</strong>
+              <time title="31 de outuburo às 18:49" dateTime="2022-10-31 18:49:30">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário"><Trash size={20} /></button>
+          </header>
+          <p>Muito bom parábens!</p>
+        </div>
+        <footer>
+          <button><ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
