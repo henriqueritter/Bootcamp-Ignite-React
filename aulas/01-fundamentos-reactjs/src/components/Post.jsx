9fcf71c1dc31442e0381aac89059a131196b1520
henriqueritter <henrique-ritter@hotmail.com>
2022-11-01 21:04:59 -0300

    styles: Add Avatar Component from Post and Sidebar

--- aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post(props) {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/henriqueritter.png" />
+          <Avatar src="https://github.com/henriqueritter.png" />
           <div className={styles.authorInfo}>
             <strong>Henrique Ritter</strong>
             <span>Web Developer</span>
--- aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styles from './Sidebar.module.css';
 
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -11,7 +12,7 @@ export function Sidebar() {
         alt="Cover Image"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/henriqueritter.png" alt="Avatar" />
+        <Avatar src="https://github.com/henriqueritter.png" />
         <strong>Henrique Ritter</strong>
         <span>Web Developer</span>
       </div>
