176af68d6984281bfd62efe58bdd102e876796c3
henriqueritter <henrique-ritter@hotmail.com>
2022-11-09 22:56:38 -0300

    fix: added typos in Comment component props

--- aulas/01-fundamentos-reactjs-ts/src/components/Comment.tsx
+++ aulas/01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -3,7 +3,12 @@ import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment({ content, onDeleteComment }) {
+interface ICommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: ICommentProps) {
   const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
@@ -19,7 +24,7 @@ export function Comment({ content, onDeleteComment }) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/henriqueritter.png" />
+      <Avatar hasBorder={false} src="https://github.com/henriqueritter.png" alt="" />
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
